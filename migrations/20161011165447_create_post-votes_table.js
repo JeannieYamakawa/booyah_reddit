5d78feaf8f72fa8d21fc4765922943142c3714e7
Timothy Chew <timothyrchew@gmail.com>
2016-10-11 17:08:46 -0500

    added votes tables migration files

---
+++ migrations/20161011165447_create_post-votes_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('comments-votes', function(table) {
+      table.increments();
+      table.integer('posts_id');
+      table.foreign('posts_id').references('posts.id');
+      table.integer('user_id');
+      table.foreign('user_id').references('users.id');
+      table.boolean('upvote').defaultTo(null);
+      table.boolean('downvote').defaultTo(null);
+      table.timestamps();
+  });
+};
+
+exports.down = function(knex, Promise) {
+
+};
