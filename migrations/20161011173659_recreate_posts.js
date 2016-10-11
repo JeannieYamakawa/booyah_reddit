dae2f8362fc0ebab4803002f57db24ec665506ec
Tosin <midastouchproductions@gmail.com>
2016-10-11 17:53:06 -0500

    fix migration

--- migrations/20161011173659_recreate_posts.js
+++ migrations/20161011173659_recreate_posts.js
@@ -1,16 +1,15 @@
-
 exports.up = function(knex, Promise) {
-   return knex.schema.createTableIfNotExists('posts', function(table) {
-    table.increments();
-    table.text('title').notNullable();
-    table.text('body').notNullable();
-    table.text('link');
-    table.integer('user_id').unsigned();
-    table.foreign('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
-    table.timestamps(true);
-  })
+    return knex.schema.createTableIfNotExists('posts', function(table) {
+        table.increments();
+        table.text('title').notNullable();
+        table.text('body').notNullable();
+        table.text('link');
+        table.integer('user_id').unsigned();
+        table.foreign('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
+        table.timestamps(true);
+    })
 };
 
 exports.down = function(knex, Promise) {
-   return knex.schema.DropTableIfExists('posts');
+    return knex.schema.dropTableIfExists('posts');
 };
