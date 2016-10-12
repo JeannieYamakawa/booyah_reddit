be53c88e2924d872a77fce8220d8efa722c3a280
Timothy Chew <timothyrchew@gmail.com>
2016-10-12 14:04:06 -0500

    add create and update comment routes

--- routes/comments.js
+++ routes/comments.js
@@ -9,7 +9,19 @@ const methodOverride = require('method-override');
 
 
 
+router.post('/users/:user_id/posts/:post_id/comment', function(req, res){
+  var comment = req.body;
+  knex('comments').insert({comment_id: comment.comment_id, user_id: comment.user_id}).then(function(){
+    res.redirect("redirect to post page");
+  });
+});
 
+router.patch('/users/:user_id/posts/:post_id/comment', function(req, res){
+  var comment = req.body;
+  knex('comments').update({comment_id: comment.comment_id, user_id: comment.user_id}).then(function(){
+    res.redirect("redirect to post page");
+  });
+});
 
 
 
