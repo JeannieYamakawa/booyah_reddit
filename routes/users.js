9f88d84cca0ff993c5af51e571303e6a4264ec29
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-12 14:44:43 -0500

    creates two users routes

--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,42 @@ const router = express.Router();
 const knex = require('../knex');
 const bcrypt = require('bcrypt-as-promised');
 
+
+
+//shows all users
 router.get('/users', function(req, res) {
-    knex('users').then((users) => {
-        res.json(users);
-
+    knex('users')
+        .then((data) => {
+        var myUsers = JSON.stringify(data)
+        //later will res.render('users/usersindex', {username: data})
+        res.send(myUsers);
     })
 });
 
 
-//Handles user registration
-router.post('/users', function(req, res) {
-    console.log(req.body);
-    bcrypt.hash(req.body.password, 10).then((hashpw) => {
-        console.log(hashpw);
-        knex('users').insert({
-            // fill your user database however you need
-        }).then((user) => {
-            console.log(user);
-            res.redirect('/users');
-        })
-
+// displays all posts by individual user
+router.get('/users/:user', function(req,res){
+    var userId = req.params.user;
+    //join table that includes posts made by users
+    knex('users').innerJoin('posts', 'users.id', 'posts.user_id').then(function(data){
+        // make an array of all the specific user's posts
+        var thisUsersPosts =[];
+        for(let i=0; i<data.length; i++){
+            if(data[i].user_id == userId){
+                thisUsersPosts.push(data[i])
+            }
+        }
+        res.send(thisUsersPosts)
     })
+})
 
-});
+
+
+
+
+
+
+
+
 
 module.exports = router;
