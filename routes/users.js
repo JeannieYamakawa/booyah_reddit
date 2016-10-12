3033a5c5a60eb75390d7931f7b2fa91a395d536d
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-10-12 15:04:20 -0500

    updates users routes

--- routes/users.js
+++ routes/users.js
@@ -34,10 +34,35 @@ router.get('/users/:user', function(req,res){
     })
 })
 
+//NEED ROUTE FOR POSTING TO /users/:user
+
+//page with form for editing user information
+router.get('/users/:user/edit',function(req,res){
+    var userRequestingEdit = req.params.user;
+    var userLoggedIn = req.session.user.username;
+    if (userRequestingEdit = userLoggedIn){
+    knex('users').where('id', userLoggedIn)
+        .then(function(data){
+
+        })
+} else{
+    res.sendStatus(401);
+}
+})
 
 
-
-
+//
+// router.post('/:id', function(req, res){
+//     var wholeComment = req.body;
+//     var iD = req.body.post_id;
+//     var postId = req.params.id;
+//     wholeComment.by_username = req.session.username;
+//     knex('comments').where('post_id', postId)
+//     .insert(wholeComment).then(function(){
+//         res.redirect('/posts/'+iD+'/comments')
+//
+//     })
+// })
 
 
 
