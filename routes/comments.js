598254967bd37818f1c2a2d0f634f02ec794c401
Taylor King <dekkofilms@gmail.com>
2016-10-12 14:15:26 -0500

    cleaned comment routes that had errors

--- routes/comments.js
+++ routes/comments.js
@@ -7,12 +7,6 @@ const knex = require('../knex');
 const bcrypt = require('bcrypt-as-promised');
 const methodOverride = require('method-override');
 
-router.delete('/users/:user_id/posts/:post_id/comment', function (res, req) {
-  knex('comments').where('id', req.body.comment_id).del().then(function () {
-    res.redirect('back');
-  });
-});
-
 router.post('/users/:user_id/posts/:post_id/comment', function(req, res){
   var comment = req.body;
   knex('comments').insert({comment_id: comment.comment_id, user_id: comment.user_id}).then(function(){
@@ -22,18 +16,15 @@ router.post('/users/:user_id/posts/:post_id/comment', function(req, res){
 
 router.patch('/users/:user_id/posts/:post_id/comment', function(req, res){
   var comment = req.body;
-  knex('comments').update({comment_id: comment.comment_id, user_id: comment.user_id}).then(function(){
+  knex('comments').where('id', req.body.comment_id).update({comment_id: comment.comment_id, user_id: comment.user_id}).then(function(){
     res.redirect("redirect to post page");
   });
 });
 
-
-
-
-
-
-
-
-
+router.delete('/users/:user_id/posts/:post_id/comment', function (res, req) {
+  knex('comments').where('id', req.body.comment_id).del().then(function () {
+    res.redirect('back');
+  });
+});
 
 module.exports = router;
