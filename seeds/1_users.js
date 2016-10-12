473f406a0b68a7334093973309bcada202a6081d
Taylor King <dekkofilms@gmail.com>
2016-10-12 16:21:02 -0500

    hashed the passwords in the seed files

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt-as-promised');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
@@ -5,10 +6,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({id: 1, username: 'Tim', hashed_password: 'password'}),
-        knex('users').insert({id: 2, username: 'Taylor', hashed_password: 'password'}),
-        knex('users').insert({id: 3, username: 'Tosin', hashed_password: 'password'}),
-        knex('users').insert({id: 4, username: 'Jeannie', hashed_password: 'password'}),
+        bcrypt.hash('password', 10).then((hashpw) => {
+            return knex('users').insert({id: 1, username: 'Tim', hashed_password: hashpw})
+        }),
+        bcrypt.hash('password', 10).then((hashpw) => {
+            return knex('users').insert({id: 2, username: 'Taylor', hashed_password: hashpw})
+        }),
+        bcrypt.hash('password', 10).then((hashpw) => {
+            return knex('users').insert({id: 3, username: 'Tosin', hashed_password: hashpw})
+        }),
+        bcrypt.hash('password', 10).then((hashpw) => {
+            return knex('users').insert({id: 4, username: 'Jeannie', hashed_password: hashpw})
+        }),
       ]);
     });
 };
