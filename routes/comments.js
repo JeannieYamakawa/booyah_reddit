1777481fc22ca4e678d79d207311b0634172b142
Taylor King <dekkofilms@gmail.com>
2016-10-12 13:30:57 -0500

    added comments route

---
+++ routes/comments.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt-as-promised');
+const methodOverride = require('method-override');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
